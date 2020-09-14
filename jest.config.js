016c7038aee51df96506d2dac6b0a0fbc372bf07
Marwa92 <engmarwayehya@yahoo.com>
2020-09-15 01:03:18 +0200

    prepare testing environment

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testPathIgnorePatterns: [
+    "/home/marwa/Projects/nextjs-boilerplate/.next/",
+    "/home/marwa/Projects/nextjs-boilerplate/node_modules/",
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$":
+      "/home/marwa/Projects/nextjs-boilerplate/node_modules/babel-jest",
+  },
+  basePath: "/src/tests",
+};
